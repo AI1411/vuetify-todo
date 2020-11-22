3518a611483cde3cac013909a78d14b1103b5295
AI1411 <goethe0719@gmail.com>
2020-11-22 18:29:54 +0900

    implement decoration of list of task

--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -1,6 +1,34 @@
 <template>
-  <div class="home pa-6">
-    <h1>Todo Page</h1>
+  <div class="home">
+    <v-list
+        class="pt-0"
+        flat
+    >
+      <div
+          v-for="task in tasks"
+          :key="task.id">
+        <v-list-item
+            @click="doneTask(task.id)"
+            :class="{ 'blue lighten-5': task.done }"
+        >
+          <template v-slot:default>
+            <v-list-item-action>
+              <v-checkbox
+                  :input-value="task.done"
+                  color="primary"
+              ></v-checkbox>
+            </v-list-item-action>
+
+            <v-list-item-content>
+              <v-list-item-title
+              :class="{ 'text-decoration-line-through': task.done }"
+              >{{ task.title }}</v-list-item-title>
+            </v-list-item-content>
+          </template>
+        </v-list-item>
+        <v-divider></v-divider>
+      </div>
+    </v-list>
   </div>
 </template>
 
@@ -8,5 +36,32 @@
 
 export default {
   name: 'Todo',
+  data() {
+    return {
+      tasks: [
+        {
+          id: 1,
+          title: "起床",
+          done: false
+        },
+        {
+          id: 2,
+          title: "朝ごはん",
+          done: false
+        },
+        {
+          id: 3,
+          title: "歯磨き",
+          done: false
+        }
+      ]
+    }
+  },
+  methods: {
+    doneTask(id) {
+      let task = this.tasks.filter(task => task.id === id)[0];
+      task.done = !task.done;
+    }
+  }
 }
 </script>
